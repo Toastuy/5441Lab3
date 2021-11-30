d36b406ff6ddd6861c4ce31d4e44acf7441314cc
Flory Logan <flory78@owens-rw02.ten.osc.edu>
2021-11-29 19:04:14 -0500

    Major part of p1 done, need to bugfix and test still

--- p1main.cu
+++ p1main.cu
@@ -1,12 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true) {
+  if (code != cudaSuccess) {
+    fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+    if (abort) exit(code);
+  }
+}
 
 // Constants
 const int MATRIXSIZE = 4000;
 
 // Serial P1
-int serialP1(){
+unsigned long serialP1(){
+
     double fMatrix [MATRIXSIZE][MATRIXSIZE];
+    unsigned long numOfSerialFlops = 0;
     int count = 0;
+
     for(int i = 0; i < MATRIXSIZE; i++){
         for(int j = 0; j < MATRIXSIZE; j++){
             // Initialize every value of the matrix
@@ -28,59 +41,97 @@ int serialP1(){
         }
     }
     printf("Total # Of Items: %d \n", count);
-    return 1;
+    return numOfSerialFlops;
 }
 
-__global__ void parallelP1(double *x, double *y){
-    // This will init array and compute all the calculations
-    // Then copy the device memory over to host memory to read from
+__global__ void cudaP1(double *A, int matrixDimension){
+
+    // TODO: Remove, checking to make sure I'm on the GPU at Owens
     printf("Hello from the GPU!");
 
-    // Matrix
-    double fMatrix [MATRIXSIZE][MATRIXSIZE];
+    // Get our thread ID
+    int blockId = blockIdx.y * gridDim.x + blockIdx.x;
+    int threadId = blockId * blockDim.x + threadIdx.x;
 
-    // Initialize memory
-    int i = threadIdx.x + blockIdx.x * blockDim.x;
-    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    // Get specific element assigned to thread
+    int column = threadId % matrixDimension;
+    int row = threadId / matrixDimension;
 
+    // Initialize each element of the array to our equations
+    // (i + j) / (double) 4000
+    A[row][column] = ((row + column) / (double) 4000);
+}
+
+unsigned long parallelP1(){
+
+    // Allocation
+    // Flops
+    unsigned long numOfParallelFlops = 0;
+
+    // Spaces in memory
+    double *d_A;
+    double *h_A = (double *) malloc(MATRIXSIZE * MATRIXSIZE * sizeof(double *));
+
+    // Cuda memory block stuff
+    int numOfBlocks = 4;
+    int threadsPerBlock = 512;
+
+    // Declare the size of our matrix
+    size_t matrixMemSize;
+    matrixMemSize = MATRIXSIZE * MATRIXSIZE * sizeof(double *);
+
+    // Malloc the spot for our array on the device
+    gpuErrchk(cudaMalloc((void**) &d_A, matrixMemSize));
+
+    // Get our dims for our cuda func call
+    dim3 dimGrid(numOfBlocks);
+    dim3 dimBlock(threadsPerBlock);
+
+    // Call our cuda function to initialize the array and do our work
+    cudaP1<<<dimGrid, dimBlock>>>(d_A, MATRIXSIZE);
+    
+    // Error checking goes here
+
+    // Copy our memory back over to the host
+    gpuErrchk(cudaMemcpy(h_A, d_A, matrixMemSize, cudaMemcpyDeviceToHost));
+
+    // Free our memory
+    gpuErrchk(cudaFree(d_A));
+
+    // Print the second element of every fourth row
+    for (int i = 0; i < MATRIXSIZE; i += 4){
+        // Check for our edge case first
+        if (i == MATRIXSIZE - 4){
+            printf("%f\n", h_A[i][1]);
+            printf("%f\n", h_A[MATRIXSIZE - 1][1]);
+        } 
+        // If I'm not an edge case, just print the element and move on
+        else {
+            printf("%f\n", h_A[i][1]);
+        }
+    }
+
+    return numOfParallelFlops;
 }
 
 int main(){
 
+    // Timing and flop stuff
+    unsigned long serialFlops = 0;
+    unsigned long parallelFlops = 0;
+    time_t start;
+    time_t finish;
+
     // Serial Way
-    serialP1();
+    time(&start);
+    serialFlops = serialP1();
+    time(&finish);
+    printf("Number of Serial Flops: %lu\n", serialFlops);
 
     // Parallel Way
-
-    // Allocation
-    double *x, *y;
-    double *d_x, *d_y;
-    x = new double(MATRIXSIZE * MATRIXSIZE);
-    y = new double(MATRIXSIZE * MATRIXSIZE);
-
-    // Thread Hierarchy
-    int nblocks = 4;
-    int tpb = 512;
-
-    // Allocate device memory
-    size_t memSize;
-    memSize = MATRIXSIZE * MATRIXSIZE * sizeof(double);
-    cudaMalloc((void**) &d_x, memSize);
-    cudaMalloc((void**) &d_y, memSize);
-
-    // Initialize memory to device
-    // TODO: Do this in GPU
-    //cudaMemcpy(d_x, x, memSize, cudaMemcpyHostToDevice);
-    //cudaMemcpy(d_y, y, memSize, cudaMemcpyHostToDevice);
-
-    // First we need to build our dims
-    dim3 dimGrid(nblocks);
-    dim3 dimBlock(tpb);
-
-    // Launch Kernal
-    parallelP1<<<dimGrid, dimBlock>>>(d_x, d_y);
-
-    cudaMemcpy(x, d_x, memSize, cudaMemcpyDeviceToHost);
-    cudaMemcpy(y, d_y, memSize, cudaMemcpyDeviceToHost);
-
+    time(&start);
+    parallelFlops = parallelP1();
+    time(&finish);
+    printf("Number of Parallel Flops: %lu\n", parallelFlops);
+    
 }
